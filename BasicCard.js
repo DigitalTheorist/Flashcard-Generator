56eeffdd725426ba7d12a9e330bdccb995ab97fa
jcostopulos <costopulos@gmail.com>
2017-07-11 17:56:21 -0700

    file structure in place

---
+++ BasicCard.js
@@ -0,0 +1,5 @@
+//Define a node module that exports a constructor for creating basic flashcards.
+//D
+//The constructor should accept two arguments (FRONT & BACK)
+//--FRONT property contains text on the front of the card.
+//--BACK property contains text on the back of the card. 
